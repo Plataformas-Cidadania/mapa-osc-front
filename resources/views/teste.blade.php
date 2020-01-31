1a8d75654e99326c031806e66992877ef29a2c83
Relison Galvao <relison@cd10.com.br>
2020-01-31 20:51:40 -0300

    Start

---
+++ resources/views/teste.blade.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="/css/app.css">
+<script src="/js/app.js"></script>
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">1</div>
+        <div class="col-md-3">2</div>
+        <div class="col-md-3"><i class="fas fa-user"></i></div>
+        <div class="col-md-3"><i class="fab fa-adobe"></i></div>
+    </div>
+</div>
