fa28db2024d83430ec79f0b6203184e9767be196
Relison Galvao <relison@cd10.com.br>
2020-02-11 23:33:31 -0300

    Menu Celular

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,21 @@
-<footer id="iniciodorodape"role="contentinfo">
-Footer
+<footer id="iniciodorodape" role="contentinfo" >
+    <div class="footer-cel d-block d-sm-none">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 col-md-4 text-center">
+                    <i class="fa fa-home" aria-hidden="true"></i>
+                    <p><a href="/">Ínicio</a></p>
+                </div>
+                <div class="col-4 col-md-4 text-center">
+                    <i class="fa fa-archive" aria-hidden="true"></i>
+                    <p><a href="/produtos">Produtos</a></p>
+                </div>
+                <div class="col-4 col-md-4 text-center">
+                    <i class="fa fa-paper-plane"></i>
+                    <p><a href="/contato">Contato</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
 
