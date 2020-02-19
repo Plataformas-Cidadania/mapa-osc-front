37002adc0250137865e55a0ae5519756c5bddf8f
Relison Andrade Galvão <b122692866@erj5119.ipea.gov.br>
2020-02-19 18:18:01 -0300

    Leaflet

---
+++ app/Http/Controllers/MapController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+
+class PageController extends Controller{
+
+    private $obj;
+    private $module;
+    private $table;
+
+
+    public function __construct(){
+        $this->obj = new \App\Page();
+        $this->module = 'page';
+        $this->table = 'pages';
+
+    }
+
+    public function details(){
+
+        $rota = Route::getCurrentRoute()->uri();
+
+        $detail = $this->obj->where('slug', $rota)->first();
+        $menus = $this->obj->orderBy('id', 'desc')->get();
+        return view($this->module.'.basic', ['detail' => $detail, 'menus' => $menus]);
+    }
+}
