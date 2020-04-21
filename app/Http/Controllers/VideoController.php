64a12dfe5bc469206382d01bb332eaa916cf69f8
Bruno Passos <brpassos.programador@gmail.com>
2020-04-21 00:33:52 -0300

    videos

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -30,20 +30,22 @@ class VideoController extends Controller{
         $lists = $this->obj
             ->join('lng_pub_videos', 'pub_videos.id', '=', 'lng_pub_videos.publish_id')
             ->select('pub_videos.*', 'lng_pub_videos.title', 'lng_pub_videos.description')
-            ->where('lng_pub_videos.publish_id', '=', 'pub_videos.id')
+            #->where('lng_pub_videos.publish_id', '=', 'pub_videos.id')
             ->orderBy('pub_videos.id', 'desc')
             ->paginate(16);
 
-
+        #Log::info($lists);
 
         //Vídeos em destaque
         $listsTop = $this->obj
             ->join('lng_pub_videos', 'pub_videos.id', '=', 'lng_pub_videos.publish_id')
             ->select('pub_videos.*', 'lng_pub_videos.title', 'lng_pub_videos.description')
-            ->where('lng_pub_videos.publish_id', 'pub_videos.id')
+            #->where('lng_pub_videos.publish_id', 'pub_videos.id')
             ->orderBy('pub_videos.id', 'desc')
             ->take(4)
             ->get();
+
+        #Log::info($listsTop);
 
         return view($this->module.'.list', ['lists' => $lists, 'listsTop' => $listsTop]);
     }
