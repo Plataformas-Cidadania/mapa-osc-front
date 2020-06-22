0e43a144c4ec03fef1a9d3f03d2f4b4177e450e1
Relison Galvão <relison@cd10.com.br>
2020-06-22 08:30:03 -0300

    Google Analytics

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,16 +8,27 @@
 <!doctype html>
 <html lang="pt-br">
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170302739-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-170302739-1');
+        </script>
+
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Nome site - @yield('title')</title>
         <base href="http://{{$base_href}}{{$barra}}">
-        {{--@include('layouts.metas')
+        @include('layouts.metas')
         @include('layouts.richCards')
-        @include('layouts.links')--}}
+        @include('layouts.links')
         @include('conexoes.css')
-     </head>
+
+    </head>
     <body {{--class="acessibilidade"--}} id="contrast" >
         @include('layouts.layout1')
     </body>
