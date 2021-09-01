fdf67e12d80b53f615b7f9e076d8af1dcee24794
Bruno Passos <brpassos.programador@gmail.com>
2021-09-01 19:40:30 -0300

    trocar senha

--- public/js/components/user-area/trocar-senha.js
+++ public/js/components/user-area/trocar-senha.js
@@ -10,9 +10,8 @@ class TrocarSenha extends React.Component {
             button: true,
             loading: false,
             requireds: {
-                tx_email_usuario: true,
-                tx_nome_usuario: true,
-                nr_cpf_usuario: true
+                senha_atual: true,
+                nova_senha: true
             },
             showMsg: false,
             msg: ''
@@ -20,32 +19,8 @@ class TrocarSenha extends React.Component {
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
-        this.register = this.register.bind(this);
         this.validate = this.validate.bind(this);
-        this.getData = this.getData.bind(this);
-    }
-
-    componentDidMount() {
-        this.getData();
-    }
-
-    getData() {
-        this.setState({ loadingCep: true, button: false });
-        $.ajax({
-            method: 'GET',
-            url: getBaseUrl2 + 'get-user-auth',
-            headers: {
-                Authorization: 'Bearer ' + localStorage.getItem('@App:token')
-            },
-            cache: false,
-            success: function (data) {
-                this.setState({ loading: false, form: data, button: true });
-            }.bind(this),
-            error: function (xhr, status, err) {
-                console.error(status, err.toString());
-                this.setState({ loadingCep: false });
-            }.bind(this)
-        });
+        this.trocarSenha = this.trocarSenha.bind(this);
     }
 
     handleInputChange(event) {
@@ -66,35 +41,11 @@ class TrocarSenha extends React.Component {
         let requireds = this.state.requireds;
         let form = this.state.form;
 
-        /*for(let index in requireds){
-            if(!form[index] || form[index]==''){
-                requireds[index] = false;
-                valid = false;
-            }else{
-                requireds[index] = true;
-            }
-        }*/
-
-        /*for(let index in requireds){
-            if(!form[index] || form[index]==''){
-                requireds[index] = false;
-                if((index==="cnpj" ) && !this.state.juridica){
-                    requireds[index] = true;
-                }else{
-                    valid = false;
-                }
-            }else{
-                requireds[index] = true;
-            }
-        }*/
-
-        //console.log(requireds);
-
         this.setState({ requireds: requireds });
         return valid;
     }
 
-    register(e) {
+    trocarSenha(e) {
         e.preventDefault();
 
         if (!this.validate()) {
@@ -103,33 +54,22 @@ class TrocarSenha extends React.Component {
 
         this.setState({ loading: true, button: false, showMsg: false, msg: '' }, function () {
             $.ajax({
-                method: 'PUT',
-                url: getBaseUrl2 + 'user/' + this.state.form.id_usuario,
+                method: 'POST',
+                url: getBaseUrl2 + 'trocar-senha-user/',
                 headers: {
                     Authorization: 'Bearer ' + localStorage.getItem('@App:token')
                 },
                 data: {
-                    tx_nome_usuario: this.state.form.tx_nome_usuario,
-                    tx_email_usuario: this.state.form.tx_email_usuario,
-                    nr_cpf_usuario: this.state.form.nr_cpf_usuario
+                    senha_atual: this.state.form.senha_atual,
+                    nova_senha: this.state.form.nova_senha
                 },
                 cache: false,
                 success: function (data) {
-
-                    let msg = 'Já existe outro cadastro com esse';
-
-                    if (data.cpf || data.email) {
-                        if (data.cpf) {
-                            msg += ' cpf';
-                        }
-                        if (data.email) {
-                            msg += ' email';
-                        }
-                        this.setState({ msg: msg, showMsg: true, loading: false, button: true });
+                    if (data.senha_atual_invalida) {
+                        this.setState({ msg: 'Senha atual inválida!', showMsg: true, loading: false, button: true });
                         return;
                     }
-
-                    msg = 'Dados alterados com sucesso!';
+                    msg = 'Senha alterada com sucesso!';
                     this.setState({ msg: msg, showMsg: true, loading: false, button: true, color: 'success' });
                 }.bind(this),
                 error: function (xhr, status, err) {
@@ -154,11 +94,6 @@ class TrocarSenha extends React.Component {
                     React.createElement('i', { className: 'fa fa-user', 'aria-hidden': 'true' }),
                     ' Meus Dados'
                 ),
-                React.createElement(
-                    'p',
-                    null,
-                    'Procure manter seu e-mail sempre atualizado. Assim, voc\xEA garante o recebimento dos comunicados, avisos e demais informa\xE7\xF5es por parte da equipe do Mapa das OSC.'
-                ),
                 React.createElement('hr', null),
                 React.createElement('br', null)
             ),
@@ -177,10 +112,10 @@ class TrocarSenha extends React.Component {
                             React.createElement(
                                 'label',
                                 { htmlFor: 'name' },
-                                'Seu nome e sobrenome*'
+                                'Senha Atual*'
                             ),
                             React.createElement('br', null),
-                            React.createElement('input', { className: "form-control form-g " + (this.state.requireds.tx_nome_usuario ? '' : 'invalid-field'), type: 'text', name: 'tx_nome_usuario', onChange: this.handleInputChange, value: this.state.form.tx_nome_usuario, placeholder: 'Nome' }),
+                            React.createElement('input', { className: "form-control form-g " + (this.state.requireds.senha_atual ? '' : 'invalid-field'), type: 'text', name: 'senha_atual', onChange: this.handleInputChange, placeholder: 'Nome' }),
                             React.createElement('br', null)
                         ),
                         React.createElement(
@@ -189,33 +124,16 @@ class TrocarSenha extends React.Component {
                             React.createElement(
                                 'label',
                                 { htmlFor: 'email' },
-                                'E-mail*'
+                                'Nova Senha*'
                             ),
                             React.createElement('br', null),
-                            React.createElement('input', { className: "form-control form-g " + (this.state.requireds.tx_email_usuario ? '' : 'invalid-field'), type: 'text', name: 'tx_email_usuario', onChange: this.handleInputChange, value: this.state.form.tx_email_usuario, placeholder: 'E-mail' }),
-                            React.createElement('br', null)
-                        ),
-                        React.createElement(
-                            'div',
-                            { className: 'col-md-4' },
-                            React.createElement(
-                                'label',
-                                { htmlFor: 'cpf' },
-                                'CPF*'
-                            ),
-                            React.createElement('br', null),
-                            React.createElement('input', { className: "form-control form-m " + (this.state.requireds.nr_cpf_usuario ? '' : 'invalid-field'), type: 'text', name: 'nr_cpf_usuario', onChange: this.handleInputChange, value: this.state.form.nr_cpf_usuario, placeholder: 'Cpf' }),
+                            React.createElement('input', { className: "form-control form-g " + (this.state.requireds.nova_senha ? '' : 'invalid-field'), type: 'text', name: 'nova_senha', onChange: this.handleInputChange, placeholder: 'E-mail' }),
                             React.createElement('br', null)
                         ),
                         React.createElement('div', { className: 'clear-float' }),
                         React.createElement(
                             'div',
                             { className: 'col-md-12' },
-                            React.createElement(
-                                'button',
-                                { style: { display: this.state.button ? 'block' : 'none' }, className: 'btn btn-success', onClick: this.register },
-                                'Salvar'
-                            ),
                             React.createElement('br', null),
                             React.createElement(
                                 'div',
--- resources/js/components/user-area/trocar-senha.js
+++ resources/js/components/user-area/trocar-senha.js
@@ -10,9 +10,8 @@ class TrocarSenha extends React.Component{
             button: true,
             loading: false,
             requireds: {
-                tx_email_usuario: true,
-                tx_nome_usuario: true,
-                nr_cpf_usuario: true,
+                senha_atual: true,
+                nova_senha: true,
             },
             showMsg: false,
             msg: '',
@@ -20,33 +19,9 @@ class TrocarSenha extends React.Component{
         };
 
         this.handleInputChange = this.handleInputChange.bind(this);
-        this.register = this.register.bind(this);
         this.validate = this.validate.bind(this);
-        this.getData = this.getData.bind(this);
+        this.trocarSenha = this.trocarSenha.bind(this);
     }
-
-    componentDidMount(){
-        this.getData();
-    }
-
-    getData(){
-            this.setState({loadingCep: true, button:false});
-            $.ajax({
-                method: 'GET',
-                url: getBaseUrl2 + 'get-user-auth',
-                headers: {
-                    Authorization: 'Bearer '+localStorage.getItem('@App:token')
-                },
-                cache: false,
-                success: function (data) {
-                    this.setState({loading: false, form: data, button:true});
-                }.bind(this),
-                error: function (xhr, status, err) {
-                    console.error(status, err.toString());
-                    this.setState({ loadingCep: false });
-                }.bind(this)
-            });
-        }
 
     handleInputChange(event) {
         const target = event.target;
@@ -66,35 +41,11 @@ class TrocarSenha extends React.Component{
         let requireds = this.state.requireds;
         let form = this.state.form;
 
-        /*for(let index in requireds){
-            if(!form[index] || form[index]==''){
-                requireds[index] = false;
-                valid = false;
-            }else{
-                requireds[index] = true;
-            }
-        }*/
-
-        /*for(let index in requireds){
-            if(!form[index] || form[index]==''){
-                requireds[index] = false;
-                if((index==="cnpj" ) && !this.state.juridica){
-                    requireds[index] = true;
-                }else{
-                    valid = false;
-                }
-            }else{
-                requireds[index] = true;
-            }
-        }*/
-
-        //console.log(requireds);
-
         this.setState({requireds: requireds});
         return valid;
     }
 
-    register(e){
+    trocarSenha(e){
         e.preventDefault();
 
         if(!this.validate()){
@@ -103,33 +54,22 @@ class TrocarSenha extends React.Component{
 
         this.setState({loading: true, button: false, showMsg: false, msg: ''}, function(){
             $.ajax({
-                method:'PUT',
-                url: getBaseUrl2+'user/'+this.state.form.id_usuario,
+                method:'POST',
+                url: getBaseUrl2+'trocar-senha-user/',
                 headers: {
                     Authorization: 'Bearer '+localStorage.getItem('@App:token')
                 },
                 data:{
-                    tx_nome_usuario: this.state.form.tx_nome_usuario,
-                    tx_email_usuario: this.state.form.tx_email_usuario,
-                    nr_cpf_usuario: this.state.form.nr_cpf_usuario,
+                    senha_atual: this.state.form.senha_atual,
+                    nova_senha: this.state.form.nova_senha,
                 },
                 cache: false,
                 success: function(data) {
-
-                    let msg = 'Já existe outro cadastro com esse';
-
-                    if(data.cpf || data.email){
-                        if(data.cpf){
-                            msg+= ' cpf';
-                        }
-                        if(data.email){
-                            msg+= ' email';
-                        }
-                        this.setState({msg: msg, showMsg: true, loading: false, button: true});
+                    if(data.senha_atual_invalida){
+                        this.setState({msg: 'Senha atual inválida!', showMsg: true, loading: false, button: true});
                         return;
                     }
-
-                    msg = 'Dados alterados com sucesso!';
+                    msg = 'Senha alterada com sucesso!';
                     this.setState({msg: msg, showMsg: true, loading: false, button: true, color: 'success'});
                 }.bind(this),
                 error: function(xhr, status, err) {
@@ -150,7 +90,6 @@ class TrocarSenha extends React.Component{
             <div>
                 <div className="title-user-area">
                     <h3><i className="fa fa-user" aria-hidden="true"/> Meus Dados</h3>
-                    <p>Procure manter seu e-mail sempre atualizado. Assim, você garante o recebimento dos comunicados, avisos e demais informações por parte da equipe do Mapa das OSC.</p>
                     <hr/>
                     <br/>
                 </div>
@@ -158,24 +97,19 @@ class TrocarSenha extends React.Component{
                     <div className="col-md-12">
                         <form>
                             <div className="col-md-8">
-                                <label htmlFor="name">Seu nome e sobrenome*</label><br/>
-                                <input className={"form-control form-g "+(this.state.requireds.tx_nome_usuario ? '' : 'invalid-field')} type="text" name="tx_nome_usuario" onChange={this.handleInputChange} value={this.state.form.tx_nome_usuario} placeholder="Nome"/><br/>
+                                <label htmlFor="name">Senha Atual*</label><br/>
+                                <input className={"form-control form-g "+(this.state.requireds.senha_atual ? '' : 'invalid-field')} type="text" name="senha_atual" onChange={this.handleInputChange} placeholder="Nome"/><br/>
                             </div>
 
                             <div className="col-md-8">
-                                <label htmlFor="email">E-mail*</label><br/>
-                                <input className={"form-control form-g "+(this.state.requireds.tx_email_usuario ? '' : 'invalid-field')} type="text" name="tx_email_usuario" onChange={this.handleInputChange} value={this.state.form.tx_email_usuario} placeholder="E-mail"/><br/>
-                            </div>
-
-                            <div className="col-md-4">
-                                <label htmlFor="cpf">CPF*</label><br/>
-                                <input className={"form-control form-m "+(this.state.requireds.nr_cpf_usuario ? '' : 'invalid-field')} type="text" name="nr_cpf_usuario" onChange={this.handleInputChange} value={this.state.form.nr_cpf_usuario} placeholder="Cpf"/><br/>
+                                <label htmlFor="email">Nova Senha*</label><br/>
+                                <input className={"form-control form-g "+(this.state.requireds.nova_senha ? '' : 'invalid-field')} type="text" name="nova_senha" onChange={this.handleInputChange} placeholder="E-mail"/><br/>
                             </div>
 
                             <div className="clear-float"/>
                             <div className="col-md-12">
 
-                                <button style={{display: this.state.button ? 'block' : 'none'}} className="btn btn-success" onClick={this.register}>Salvar</button>
+                                {/*<button style={{display: this.state.button ? 'block' : 'none'}} className="btn btn-success" onClick={this.register}>Salvar</button>*/}
                                 <br/>
                                 <div style={{display: this.state.showMsg ? 'block' : 'none'}} className={'text-'+this.state.color}>{this.state.msg}</div>
                                 <div style={{display: this.state.loading ? 'block' : 'none'}}><i className="fa fa-spin fa-spinner"/>Processando</div>
