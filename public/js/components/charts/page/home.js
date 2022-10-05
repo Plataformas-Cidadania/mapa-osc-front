6c22e0e110bb5e87c7e9ae023bcb22ccfcdd6a20
Bruno Passos <brpassos.programador@gmail.com>
2022-10-05 16:14:55 -0300

    graficos home

--- public/js/components/charts/page/home.js
+++ public/js/components/charts/page/home.js
@@ -13,29 +13,52 @@ class Home extends React.Component {
 
     load() {
         let _this = this;
-
-        let charts = [9, 10, 11, 12];
+        //let charts = [9,10,11,12]
 
         let data = _this.state.data;
-        data = this.loadCharts(charts, 0, data);
+
+        //data = this.loadCharts(charts, 0, data);
+
         //console.log(data);
-        this.setState({ data: data });
+        //this.setState({data: data});
+        $.ajax({
+            method: 'GET',
+            //url: getBaseUrl2+'osc/grafico/'+charts[i],
+            url: 'indicadores/analises/',
+            data: {},
+            cache: false,
+            async: false,
+            success: function (result) {
+                //console.log("=============", result);
+                _this.loadCharts(result, 0, data);
+            },
+            error: function (xhr, status, err) {
+                console.error(status, err.toString());
+                _this.setState({ loading: false });
+            }
+        });
     }
 
     loadCharts(charts, i, data) {
         let _this = this;
         $.ajax({
             method: 'GET',
-            url: getBaseUrl2 + 'osc/grafico/' + charts[i],
+            //url: getBaseUrl2+'osc/grafico/'+charts[i],
+            url: getBaseUrl2 + 'osc/grafico/' + charts[i]['id_analise'],
             data: {},
             cache: false,
             async: false,
             success: function (result) {
                 data.push(result);
                 i++;
-                if (i < charts.length) {
+                /*if(i < charts.length){
                     data = _this.loadCharts(charts, i, data);
-                }
+                }*/
+                _this.setState({ data: data }, function () {
+                    if (i < charts.length) {
+                        _this.loadCharts(charts, i, data);
+                    }
+                });
             },
             error: function (xhr, status, err) {
                 console.error(status, err.toString());
