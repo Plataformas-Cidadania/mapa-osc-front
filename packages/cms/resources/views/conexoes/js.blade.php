a2d18b984c4f58138f27a445fcbd5497cc106e11
Relison Galvão <relison.galvao@impa.br>
2024-07-25 15:12:58 -0300

    MOSC-2616

--- packages/cms/resources/views/conexoes/js.blade.php
+++ packages/cms/resources/views/conexoes/js.blade.php
@@ -32,6 +32,10 @@ $base_href = config('app.url');
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
 <![endif]-->
-<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+{{--<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>--}}
+
+
+<script src="/js/lib/html5shiv.min.js"></script>
+<script src="/js/lib/respond.min.js"></script>
 
