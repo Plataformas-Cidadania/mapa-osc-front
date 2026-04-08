85c2af5e721f8592b7e44e10a066423c95280871
Relison Galvão <relison@cd10.com.br>
2026-04-08 15:40:52 -0300

    MOSC-2836

--- resources/views/osc/detail.blade.php
+++ resources/views/osc/detail.blade.php
@@ -46,7 +46,7 @@
     ?>
 
 
-     @if($status->bo_osc_ativa === false)
+     @if(isset($status->bo_osc_ativa) && $status->bo_osc_ativa == false)
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
